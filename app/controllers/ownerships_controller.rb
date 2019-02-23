90f8fce5bd4a5db9b3e7e637e14e962d9ab98f33
Taimo <taimo1999.5@gmail.com>
2019-02-23 17:15:00 +0900

    have

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -9,7 +9,10 @@ class OwnershipsController < ApplicationController
 
     if params[:type] == 'Want'
       current_user.want(@item)
-      flash[:success] = '商品をwantしました。'
+      flash[:success] = '商品を want しました。'
+    elsif params[:type] == 'Have'
+      current_user.have(@item)
+      flash[:success] = '商品を Have しました。'
     end
 
     redirect_back(fallback_location: root_path)
@@ -20,6 +23,9 @@ class OwnershipsController < ApplicationController
      if params[:type] == 'Want'
        current_user.unwant(@item)
        flash[:success] = '商品の Want を解除しました。'
+     elsif params[:type] == 'Have'
+       current_user.unhave(@item)
+       flash[:success] = '商品の Have を解除しました。'
      end
     redirect_back(fallback_location: root_path)
   end
